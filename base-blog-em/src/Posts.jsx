63daca505e03172d66e41f7d6a201f5cfce12791
김보미 <kimbomi2172@naver.com>
2024-07-22 14:14:35 +0900

    ♻️ refactor: 페이지네이션 데이터 프리패칭

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { fetchPosts, deletePost, updatePost } from './api'
 import { PostDetail } from './PostDetail'
 const maxPostPage = 10
@@ -7,6 +7,19 @@ const maxPostPage = 10
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(1)
   const [selectedPost, setSelectedPost] = useState(null)
+
+  const queryClient = useQueryClient()
+
+  // prefetching으로 페이지네이션 로딩시간 줄이기
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1
+      queryClient.prefetchQuery({
+        queryKey: ['posts', nextPage],
+        queryFn: () => fetchPosts(nextPage),
+      })
+    }
+  }, [currentPage, queryClient])
 
   // replace with useQuery
   const { data, isError, error, isLoading } = useQuery({
